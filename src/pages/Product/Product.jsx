44324f0f0d77e3849d136625a60b453d6d6e6cbd
Surya <surya.tectum@gmail.com>
2023-01-27 15:54:42 +0530

    adding product css changes

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,6 +1,6 @@
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useMediaQuery } from "react-responsive";
@@ -35,13 +35,12 @@ const Product = () => {
           />
         </div>
       </div>
-      <center>
-        <div
+      
+        {/* <div
           className="flex"
           style={{ flexDirection: "row", justifyContent: "space-evenly" }}
         >
           {data?.attributes?.images?.data?.map((item, index) => { return <img className="img-mob" src={item.attributes.url} alt="" onClick={(e) => setSelectedImg(item.attributes.url)} /> })}
-
           <div>
         <img
           className="img-mob"
@@ -58,8 +57,28 @@ const Product = () => {
           onClick={(e) => setSelectedImg("img2")}
         />
         </div>
-        </div>
-      </center>
+        </div> */}
+        <Grid container spacing={2}>
+          {data?.attributes?.images?.data?.map((item, index) => { return <Grid item sm={4}>  <img className="img-mob" src={item.attributes.url} alt="" onClick={(e) => setSelectedImg(item.attributes.url)} /> </Grid>})}
+          
+          <Grid item sm={4}>
+          <img
+          className="img-mob"
+          src={data?.attributes?.img?.data?.attributes?.url}
+          alt="product-image"
+          onClick={(e) => setSelectedImg("img")}
+        />
+          </Grid>
+          <Grid item sm={4}>
+          <img
+          className="img-mob"
+          src={data?.attributes?.img2?.data?.attributes?.url}
+          alt=""
+          onClick={(e) => setSelectedImg("img2")}
+        />
+          </Grid>
+          
+        </Grid>
 
     </div>
   );
@@ -95,12 +114,12 @@ const Product = () => {
           <div className="left">
             {isTabletOrMobile ? renderForMobile() : renderForDesktop()}
           </div>
-          <div className="right" style={{ paddingLeft: 55 }}>
+          <div className="right" >
             {/* <h1 class="productTitle">{data?.attributes?.title}</h1> */}
             <Typography gutterBottom align="center" class="productTitle" color='inherit' >{data?.attributes?.title}</Typography>
             <span className="price">₹{data?.attributes?.price}</span>
 
-            <div>
+            <div style={{display:"flex", gap:20}}>
 
               <label htmlFor="size" style={{ marginRight: 10 }}>Select Size</label >
               <select
@@ -115,11 +134,11 @@ const Product = () => {
                   </option>
                 )) : ''}
               </select>
-              <div> <PopupImageButton data={data}/></div>
+              <div > <PopupImageButton data={data}/></div>
             </div>
 
             <p>{data?.attributes?.desc}</p>
-            <div className="quantity" style={{ paddingLeft: 55 }}>
+            <div className="quantity" style={{ paddingLeft: 5 }}>
               <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
               <div class="quantityCenter"> <b> {quantity}</b></div>
               <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
@@ -172,158 +191,3 @@ const Product = () => {
 };
 
 export default Product;
-
-
-   
-//    <div className="left" style={{ flexDirection: "column" }}>
-//       <div className="container-main-img">
-//         <div className="main-img-mob">
-//           <img
-//             src={data?.attributes[selectedImg]?.data?.attributes?.url}
-//             alt=""
-//           />
-//         </div>
-//       </div>
-//       <center>
-//       <div
-//         className="flex"
-//         style={{ flexDirection: "row", justifyContent: "space-evenly" }}
-//       >
-//         <div>
-//         <img
-//           className="img-mob"
-//           src={data?.attributes?.img?.data?.attributes?.url}
-//           alt="product-image"
-//           onClick={(e) => setSelectedImg("img")}
-//         />
-//         </div>
-//         <div>
-//         <img
-//           className="img-mob"
-//           src={data?.attributes?.img2?.data?.attributes?.url}
-//           alt=""
-//           onClick={(e) => setSelectedImg("img2")}
-//         />
-//         </div>
-     
-        
-        
-//       </div>
-//       </center>
-      
-//     </div>
-//   );
-//   const renderForDesktop = () => (
-//     <>
-//       <div className="images">
-//         <img
-//           src={data?.attributes?.img?.data?.attributes?.url}
-//           alt=""
-//           onClick={(e) => setSelectedImg("img")}
-//         />
-//         <img
-//           src={data?.attributes?.img2?.data?.attributes?.url}
-//           alt=""
-//           onClick={(e) => setSelectedImg("img2")}
-//         />
-//         <img
-//           src={data?.attributes?.images?.data[0]?.attributes?.url}
-//           alt=""
-//           onClick={(e) => setSelectedImg("img")}
-//         />
-//       </div>
-//       <div className="mainImg">
-//         <img
-//           src={data?.attributes[selectedImg]?.data?.attributes?.url}
-//           alt=""
-//         />
-//       </div>
-//     </>
-//   );
-//   return (
-//     <div className="product">
-//       {loading ? (
-//         "loading"
-//       ) : (
-//         <>
-//           <div className="left">
-//             {isTabletOrMobile ? renderForMobile() : renderForDesktop()}
-//           </div>
-//           <div className="right" style={{ paddingLeft:55 }}>
-//             {/* <h1 class="productTitle">{data?.attributes?.title}</h1> */}
-//             <Typography gutterBottom align="center" class="productTitle" color='inherit' >{data?.attributes?.title}</Typography>
-//             <span className="price">₹{data?.attributes?.price}</span>
-
-//             <div>
-             
-//               <label htmlFor="size" style={{marginRight: 10}}>Select Size</label >
-//               <select
-//                 id="size"
-//                 value={selectedSize}
-//                 onChange={(e) => setSelectedSize(e.target.value)}
-//               >
-
-//                 <option value="">Select a size</option>
-//                 { data != null ? Object.keys(data?.attributes?.Size?.size).map((size) => (
-//                   <option key={size} value={size}>
-//                     {size}
-//                   </option>
-//                 )) :'' }
-//               </select>
-//               <div>  <PopupImageButton /></div>
-//             </div>
-
-//             <p>{data?.attributes?.desc}</p>
-//             <div className="quantity" style={{ paddingLeft:55 }}>
-//               <button onClick={() =>setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
-//              <div class="quantityCenter"> <b> {quantity}</b></div>
-//               <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-//             </div>
-//             <button 
-//               className="add"
-//               onClick={() =>
-//                 dispatch(
-//                   addToCart({
-//                     id: data.id,
-//                     title: data.attributes.title,
-//                     desc: data.attributes.desc,
-//                     price: data.attributes.price,
-//                     img: data.attributes.img.data.attributes.url,
-//                     quantity,
-//                   })
-//                 )
-//               }
-//             >
-//               <AddShoppingCartIcon /> ADD TO CART
-//             </button>
-//             {/* <div className="links"  style={{ paddingLeft:55 }}>
-//               <div className="item">
-//                 <FavoriteBorderIcon /> ADD TO WISH LIST
-//               </div> */}
-//               {/* <div className="item">
-//                 <BalanceIcon /> ADD TO COMPARE
-//               </div> */}
-//             {/* </div> */}
-//             <div className="info" >
-//               {/* <span>Vendor: Polo</span> */}
-//               {/* <span>Product Type: T-Shirt</span> */}
-//               {/* <span>Tag: T-Shirt, Women, Top</span> */}
-//             </div>
-//             <hr />
-//             <div className="info">
-//               <span>{data?.attributes?.description}</span>
-//               <span>{data?.attributes?.desc}</span>
-//               <hr />
-//               <span>ADDITIONAL INFORMATION</span>
-//               <hr />
-//               <span>FAQ</span>
-//             </div>
-//           </div>
-//           <ShareIcons />
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Product;
